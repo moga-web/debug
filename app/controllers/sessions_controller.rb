8233aafe0fa6ed89eed3ece77e7d520d513e07ce
mogami <ndarfc.57@gmail.com>
2023-08-24 23:32:06 +0900

    fix login_function2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-    binding.break
+    user = User.find_by(email: params[:sessions][:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to posts_path, notice: 'ログインしました'
